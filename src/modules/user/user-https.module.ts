bb7e270cf1efe8eb951729356930b4fa4eb3d2e7
pawelzaborowski <pawel.zaborowski@neoteric.eu>
2021-09-06 13:04:59 +0200

    chore(): create postgres config & add gitignore

--- src/modules/user/user-https.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsersModule } from './users.module';
-import { UsersService } from './users.service';
-
-@Module({
-  imports: [UsersModule],
-  providers: [UsersService],
-})
-export class UserHttpModule {}
