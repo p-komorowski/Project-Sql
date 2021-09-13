f98dc1fcc72b56bb099d5b0ba30af019d3dcd5f8
p-komorowski <pkomorowski29@gmail.com>
2021-09-13 17:48:03 +0200

    feat(basket.controller): add guards

--- src/modules/basket/basket.controller.ts
+++ src/modules/basket/basket.controller.ts
@@ -1,20 +1,25 @@
-import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, UseGuards } from "@nestjs/common";
+import { JwtAuthGuard } from "../auth/strategy/jwt-auth.guard";
 import { BasketService } from "./basket.service";
-import { shoppingBasket } from "./basket_entity/shopping_basket.entity";
+import { shoppingBasket } from "./entity/shopping_basket.entity";
 
 @Controller("basket")
 export class BasketController {
   constructor(private basketService: BasketService) {}
+  
+  @UseGuards(JwtAuthGuard)
   @Get()
   async getAllProducts(): Promise<shoppingBasket[]> {
     return await this.basketService.getProducts();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   async addProduct(@Body() productData: shoppingBasket): Promise<string> {
     return this.basketService.insertProduct(productData);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(":basket_id")
   async removeProduct(@Param("basket_id") basket_id: string) {
     await this.basketService.deleteProduct(basket_id);
