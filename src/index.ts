f57f9960ef203db7c0b933a48c0e478e0117bf9d
p-komorowski <pkomorowski29@gmail.com>
2021-09-13 17:54:44 +0200

    refactor(index): change username to existing one

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
 import {Token} from "./modules/auth/entity/token.entity";
-import {shoppingBasket} from "./modules/basket/basket_entity/shopping_basket.entity";
-import {Books} from "./modules/books/books_entity/books.entity";
+import {shoppingBasket} from "./modules/basket/entity/shopping_basket.entity";
+import {Books} from "./modules/books/entity/books.entity";
 import {ContactDetails, User} from "./modules/user/entities";
 
 createConnection({
-  type: "postgres",
+  type: 'postgres',
   host: "localhost",
   port: 5432,
-  username: "test",
-  password: "test",
-  database: "project2",
+  username: "postgres",
+  password: "Haslo!234",
+  database: "Project2",
   entities: [User, ContactDetails, Books, shoppingBasket, Token],
   synchronize: true,
   logging: false,
