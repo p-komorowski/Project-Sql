036aeb6a2d1bbdda528a8218756fc4feb3e41fb0
p-komorowski <pkomorowski29@gmail.com>
2021-09-15 12:36:10 +0200

    feat(basket.module): add controller

--- src/modules/basket/basket.module.ts
+++ src/modules/basket/basket.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Books } from "../books/entity/books.entity";
+import { BasketController } from "./basket.controller";
 import { BasketService } from "./basket.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Books])],
   providers: [BasketService],
+  controllers:[BasketController]
 })
 export class BasketModule {}
