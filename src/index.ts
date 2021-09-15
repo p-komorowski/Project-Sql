3dd621e5eedc14a64ea5346b9bbc2f0eb501d76f
p-komorowski <pkomorowski29@gmail.com>
2021-09-15 12:45:38 +0200

    refactor(index): delete unused imports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {Token} from "./modules/auth/entity/token.entity";
-import {shoppingBasket} from "./modules/basket/entity/shopping_basket.entity";
-import {Books} from "./modules/books/entity/books.entity";
-import {ContactDetails, User} from "./modules/user/entities";
-
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
