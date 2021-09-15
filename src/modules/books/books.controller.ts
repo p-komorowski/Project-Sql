d0067fc700d3f34c4a1f125288f4fc8a1c1c0d85
p-komorowski <pkomorowski29@gmail.com>
2021-09-15 12:37:02 +0200

    refactor(books.controller): remove guards

--- src/modules/books/books.controller.ts
+++ src/modules/books/books.controller.ts
@@ -1,20 +1,25 @@
-import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, UseGuards } from "@nestjs/common";
 import { Books } from "./entity/books.entity";
 import { BooksService } from "./books.service";
+import { JwtAuthGuard } from "../auth/strategy/jwt-auth.guard";
 
-@Controller("books")
+@Controller('books')
 export class BooksController {
   constructor(private booksService: BooksService) {}
+
+  
   @Get()
   async getAllProducts(): Promise<Books[]> {
     return await this.booksService.getProducts();
   }
 
+  
   @Post()
   async addProduct(@Body() productData: Books): Promise<Books> {
     return this.booksService.insertProduct(productData);
   }
 
+  
   @Delete("/:IBSN")
   async removeProduct(
       @Param("IBSN") IBSN: string
