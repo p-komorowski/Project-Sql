935e1f0888150ae22e346749d030942d9e0b4cf4
p-komorowski <pkomorowski29@gmail.com>
2021-09-15 12:42:52 +0200

    refactor(app.module): add controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import {Module} from "@nestjs/common";
 import {TypeOrmModule} from "@nestjs/typeorm";
+import { AppController } from "./app.controller";
 import {config} from "./config";
+import { AuthModule } from "./modules/auth/auth.module";
+import { BooksModule } from "./modules/books/books.module";
+import { UserModule } from "./modules/user/user.module";
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(config.database), // import samego configu zamiast pisania calej konfiguracji
+    TypeOrmModule.forRoot(config.database),
+    UserModule,
+    BooksModule, // import samego configu zamiast pisania calej konfiguracji
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
