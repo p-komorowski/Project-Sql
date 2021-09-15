ffd11d233cfd87d77827f7aaded0333392a95b37
p-komorowski <pkomorowski29@gmail.com>
2021-09-15 12:37:40 +0200

    feat(books.module): add controller

--- src/modules/books/books.module.ts
+++ src/modules/books/books.module.ts
@@ -1,11 +1,19 @@
-import {Module} from "@nestjs/common";
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {BooksService} from "./books.service";
-import {entities} from "../../config";
-import {BooksRepository} from "./repository/books.repository";
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { BooksService } from "./books.service";
+import { entities } from "../../config";
+import { BooksRepository } from "./repository/books.repository";
+import { AuthModule } from "../auth/auth.module";
+import { UserModule } from "../user/user.module";
+import { BooksController } from "./books.controller";
 
 @Module({
-  imports: [TypeOrmModule.forFeature(entities)],
+  imports: [
+    TypeOrmModule.forFeature(entities),
+    UserModule,
+    BooksModule,
+  ],
+  controllers: [BooksController],
   providers: [BooksService, BooksRepository],
 })
 export class BooksModule {}
