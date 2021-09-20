12364ef1af73d7e3a455b92047f3a17539b6710d
p-komorowski <pkomorowski29@gmail.com>
2021-09-20 12:31:20 +0200

    refactor(@Module): add AuthModule and Basket Module to imports, add AuthRepository to providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,20 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import {config} from "./config";
 import { AuthModule } from "./modules/auth/auth.module";
+import { AuthRepository } from "./modules/auth/repository/auth.repository";
+import { BasketModule } from "./modules/basket/basket.module";
 import { BooksModule } from "./modules/books/books.module";
 import { UserModule } from "./modules/user/user.module";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(config.database),
+    AuthModule,
     UserModule,
-    BooksModule, // import samego configu zamiast pisania calej konfiguracji
+    BooksModule,
+    BasketModule, // import samego configu zamiast pisania calej konfiguracji
   ],
   controllers: [AppController],
+  providers:[AuthRepository]
 })
 export class AppModule {}
