b2754cd0da9bfee4335529e7f2dd02db4215c804
p-komorowski <pkomorowski29@gmail.com>
2021-09-20 12:21:36 +0200

    refactor(basket.repository): change directory to shoppginBasket

--- src/modules/basket/repository/basket.repository.ts
+++ src/modules/basket/repository/basket.repository.ts
@@ -1,5 +1,5 @@
 import { EntityRepository, Repository } from "typeorm";
-import { shoppingBasket } from "../entity/shopping_basket.entity";
+import { shoppingBasket } from "../entity/basket.entity";
 
 @EntityRepository(shoppingBasket)
 export class BasketRepository extends Repository<shoppingBasket> {}
