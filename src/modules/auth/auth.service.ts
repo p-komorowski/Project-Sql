f388f086ab465640d48d667ac16e6dd1c8493f80
p-komorowski <pkomorowski29@gmail.com>
2021-09-20 12:17:31 +0200

    refactor(auth.service): use typeorm connection instead of inject model

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,26 +1,31 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import * as bcrypt from "bcrypt";
+import { Connection } from "typeorm";
 import { LoginDto } from "./dto/login.dto";
 import { RegisterDto } from "./dto/register.dto";
 import { AuthRepository } from "./repository/auth.repository";
 
 @Injectable()
 export class AuthService {
+  private authRepository: AuthRepository;
   constructor(
     private jwtService: JwtService,
-    private readonly repository: AuthRepository
-  ) {}
+      private readonly connection: Connection
+  ) {
+      this.authRepository = this.connection.getCustomRepository(AuthRepository);
+  }
 
   async validateUser(email: string, pass: string): Promise<any> {
-    const user = await this.repository.findOne(email);
+    const user = await this.authRepository.findOne(email);
     if (user && (await bcrypt.compare(pass, user.password))) {
       const { password, ...result } = user;
       return result;
     }
   }
+
   async register(userData: RegisterDto): Promise<any> {
-    return this.repository.create(userData);
+    return this.authRepository.create(userData);
   }
   async login(user: LoginDto): Promise<any> {
     const payload = { sub: user.email, pass: user.password };
