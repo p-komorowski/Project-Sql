acb42ea4b2e09418ae21cb3ebc768c2fa3cc58b1
p-komorowski <pkomorowski29@gmail.com>
2021-09-20 12:28:32 +0200

    refactor(addProduct): use BookDto class

--- src/modules/books/books.controller.ts
+++ src/modules/books/books.controller.ts
@@ -2,24 +2,26 @@ import { Body, Controller, Delete, Get, Param, Post, UseGuards } from "@nestjs/c
 import { Books } from "./entity/books.entity";
 import { BooksService } from "./books.service";
 import { JwtAuthGuard } from "../auth/strategy/jwt-auth.guard";
+import { BookDto } from "./dto/books.dto";
 
 @Controller('books')
 export class BooksController {
   constructor(private booksService: BooksService) {}
 
-  
+  @UseGuards(JwtAuthGuard)
   @Get()
   async getAllProducts(): Promise<Books[]> {
     return await this.booksService.getProducts();
   }
 
-  
+  @UseGuards(JwtAuthGuard)
   @Post()
-  async addProduct(@Body() productData: Books): Promise<Books> {
+  async addProduct(@Body() productData: BookDto): Promise<Books> {
+    console.log(productData)
     return this.booksService.insertProduct(productData);
   }
 
-  
+  @UseGuards(JwtAuthGuard)
   @Delete("/:IBSN")
   async removeProduct(
       @Param("IBSN") IBSN: string
