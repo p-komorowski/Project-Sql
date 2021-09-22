3f64b8b5898f0a7d66f538440a22d6620e18ac4d
p-komorowski <pkomorowski29@gmail.com>
2021-09-22 11:50:52 +0200

    style(BookDto): change style

--- src/modules/books/dto/books.dto.ts
+++ src/modules/books/dto/books.dto.ts
@@ -1,26 +1,25 @@
 import { Exclude, Expose } from "class-transformer";
-import { IsNumber, isNumber, IsString } from "class-validator";
+import { IsNumber, IsString } from "class-validator";
 
 @Exclude()
 export class BookDto {
+  @Expose()
+  @IsNumber()
+  IBSN: number;
 
-@Expose()
-@IsNumber()
- IBSN: number;
+  @Expose()
+  @IsString()
+  title: string;
 
- @Expose()
-@IsString()
- title: string;
+  @Expose()
+  @IsNumber()
+  price: number;
 
- @Expose()
- @IsNumber()
- price: number;
+  @Expose()
+  @IsString()
+  author: string;
 
- @Expose()
- @IsString()
- author: string;
-
- @Expose()
- @IsNumber()
- count: number;
+  @Expose()
+  @IsNumber()
+  count: number;
 }
