5c4b9fd2fb7e41371ee2bb9cef24d294cbde9853
p-komorowski <pkomorowski29@gmail.com>
2021-09-23 16:28:24 +0200

    refactor:add new method AddNewToken

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,57 +1,74 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import * as bcrypt from "bcrypt";
+import { now } from "mongoose";
 import { Connection } from "typeorm";
+import { User } from "../user/entities";
+import { UsersService } from "../user/users.service";
 import { LoginDto } from "./dto/login.dto";
 import { RegisterDto } from "./dto/register.dto";
+import { Token } from "./entity/token.entity";
 import { AuthRepository } from "./repository/auth.repository";
 
 @Injectable()
 export class AuthService {
   private authRepository: AuthRepository;
   constructor(
+    private userService: UsersService,
     private jwtService: JwtService,
       private readonly connection: Connection
   ) {
       this.authRepository = this.connection.getCustomRepository(AuthRepository);
   }
 
-  async validateUser(email: string, pass: string): Promise<any> {
-    const user = await this.authRepository.findOne(email)
-    console.log(user)
-    console.log(email, pass)
+  async validateUser(user:User, pass:string): Promise<any> {
     if (user && (await bcrypt.compare(pass, user.password))) {
       const {password, ...result} = user;
       return result;
     }
     else {
-      throw new NotFoundException('validateUser')
+      throw new NotFoundException('Incorrect password or email')
     }
   }
 
   async register(userData: RegisterDto): Promise<any> {
-    const userReg = await this.authRepository.findOne(userData.email)
-    console.log(userReg)
+    const userReg = await this.userService.findByEmail(userData.email)
     if (!userReg){
     const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(userData.password, salt);
       userData.password = hashedPassword
-    return this.authRepository.save(userData)
+      const newUser = this.userService.create(userData);
   }
     else {
-      throw new NotFoundException ('email already in database')
+      throw new BadRequestException ('email already in database')
     }
   }
 
   async login(user: LoginDto): Promise<any> {
     const payload = { sub: user.email, pass: user.password };
-    const valUser = await this.validateUser(user.email, user.password); 
+    const userEntity = await this.userService.findByEmail(user.email)
+    const valUser = await this.validateUser(userEntity, user.password); 
+    const token = await this.jwtService.sign(payload)
+    const newToken = await this.addNewToken(userEntity,token)
      if (valUser) {
       return {
-        access_token: this.jwtService.sign(payload),
+        token
       };
     }  else {
       throw new NotFoundException('cannot validate');
     }
   } 
+
+  async addNewToken(user: User, jwt: string): Promise <any>{
+    const newTime = new Date();
+    const time = new Date (newTime.getTime() + 60000*10);
+    const addedToken = new Token({
+      token: jwt,
+      expTime: time,
+      lastLogin: newTime,
+      user: user
+    });
+    this.authRepository.create(addedToken)
+    return this.authRepository.save(addedToken)
+  }
 }
