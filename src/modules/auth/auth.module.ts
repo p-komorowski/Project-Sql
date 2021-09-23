8954f58226bc8de30736d44485033194578de33a
p-komorowski <pkomorowski29@gmail.com>
2021-09-23 16:27:32 +0200

    refactor(singOptions):change time  to 600s

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,5 +1,5 @@
 import { Module } from "@nestjs/common";
-import { JwtModule, JwtService } from "@nestjs/jwt";
+import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 import { AuthService } from "./auth.service";
 import { JwtStrategy } from "./strategy/jwt.strategy";
@@ -7,19 +7,21 @@ import { LocalStrategy } from "./strategy/local.strategy";
 import { UserModule } from "../user/user.module";
 import { AuthController } from "./auth.controller";
 import { AuthRepository } from "./repository/auth.repository";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   imports: [
+    TypeOrmModule,
     AuthModule,
     UserModule,
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: "1200s" },
+      signOptions: { expiresIn: "600s" },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, JwtStrategy,AuthRepository],
+  providers: [AuthService, LocalStrategy, JwtStrategy, AuthRepository],
   exports: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
