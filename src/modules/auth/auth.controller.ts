aa78051443b223326a193a4debd4af43b7fd6bd1
p-komorowski <pkomorowski29@gmail.com>
2021-09-23 17:56:21 +0200

    [WIP]feat(post(auth/login): implement cookies

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import { Controller, Post, Body, Res, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { LoginDto } from "./dto/login.dto";
 import { RegisterDto } from "./dto/register.dto";
+import { Response } from 'express';
+import { AuthGuard } from "@nestjs/passport";
 
 @Controller()
 export class AuthController {
@@ -13,7 +15,22 @@ export class AuthController {
   }
 
   @Post("auth/login")
-  async login(@Body() loginDto: LoginDto) {
-    return this.authService.login(loginDto)
+  async login(@Body() loginDto: LoginDto,response: Response) {
+   const cookie = this.authService.login(loginDto)
+   response
+      .cookie('access_token', cookie, {
+        httpOnly: true,
+        domain: 'localhost:3000', 
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
+      })
+      .send({ success: true })
+      
   }
+
+  @Post('hello')
+  @UseGuards(AuthGuard('jwt'))
+  devices(): string {
+    return 'Hello ';
+  }
+
 }
