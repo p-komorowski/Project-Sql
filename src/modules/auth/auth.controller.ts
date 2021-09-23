202a0f329a6a181d68c53df23bf45f14a37c5022
p-komorowski <pkomorowski29@gmail.com>
2021-09-23 16:25:40 +0200

    :feat(auth): add auth module

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -14,6 +14,6 @@ export class AuthController {
 
   @Post("auth/login")
   async login(@Body() loginDto: LoginDto) {
-    return this.authService.login(loginDto);
+    return this.authService.login(loginDto)
   }
 }
