056ddf61b24e000d7686867e9c60800c22112809
p-komorowski <pkomorowski29@gmail.com>
2021-09-23 16:22:37 +0200

    refactor(password): delete column password

--- src/modules/auth/entity/token.entity.ts
+++ src/modules/auth/entity/token.entity.ts
@@ -1,25 +1,27 @@
-import {Entity, Column, PrimaryColumn} from "typeorm";
+import { User } from "../../user/entities";
+import {Entity, Column, PrimaryColumn, ManyToOne, JoinColumn, DeepPartial} from "typeorm";
 import { v4 as uuid } from 'uuid';
+import { TokenInterface } from "../interface/token.interface";
 
 @Entity()
 export class Token {
+    constructor(token: DeepPartial<TokenInterface>) {
+        Object.assign(this, token);
+      }
 
     @PrimaryColumn()
     id: string = uuid();
-
-    @Column()
-    userId: string = uuid();
-
+   
+    @ManyToOne(() => User, (user) => user.id)
+    @JoinColumn({name: 'userId'})
+    user:User  
+   
     @Column()
     token: string;
 
     @Column()
-    expTime: string;
+    expTime: Date;
 
     @Column()
-    lastLogin: string;
-
-    @Column()
-    password: string;
-
+    lastLogin: Date;
 }
