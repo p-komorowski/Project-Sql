5de282c9ce5af67b68c7c9f97cf1ba76b021689e
p-komorowski <pkomorowski29@gmail.com>
2021-09-23 16:32:45 +0200

    refacotr(providers): add userRepository to providers

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
+import { userRepository } from './repository/user.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService],
+  providers: [UsersService,userRepository],
   exports:[UsersService],
 })
 export class UserModule {}
