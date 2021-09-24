54bc8d6d5d13e3d61f25fe41d78269f7aeaba256
p-komorowski <pkomorowski29@gmail.com>
2021-09-24 13:19:11 +0200

    style(*):better looking code

--- src/modules/basket/dto/basket.dto.ts
+++ src/modules/basket/dto/basket.dto.ts
@@ -1,13 +1,11 @@
 import { Exclude, Expose } from "class-transformer";
-import { IsNumber,} from "class-validator";
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 
 @Exclude()
 export class BasketDto {
+  @Expose()
+  basketId: string = uuid();
 
-@Expose()
- basketId: string = uuid()
-
- @Expose()
- userId: string = uuid()
+  @Expose()
+  userId: string = uuid();
 }
