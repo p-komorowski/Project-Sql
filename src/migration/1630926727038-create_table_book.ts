b2e10ef5e6b1fc6cb0e51216c3734d783b8acadf
p-komorowski <pkomorowski29@gmail.com>
2021-09-24 13:12:37 +0200

    style(*):better looking code

--- src/migration/1630926727038-create_table_book.ts
+++ src/migration/1630926727038-create_table_book.ts
@@ -1,9 +1,9 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class createTableBook1630926727038 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> { // sukces
-        await queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // sukces
+    await queryRunner.query(`
             CREATE TABLE IF NOT EXISTS book ( 
                 IBSN serial PRIMARY KEY,
                 title VARCHAR NOT NULL,
@@ -11,13 +11,12 @@ export class createTableBook1630926727038 implements MigrationInterface {
                 ...
                 )
         `); // <- kod SQLowy
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {  // rollback
-        await queryRunner.query(`
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // rollback
+    await queryRunner.query(`
         DROP TABLE book
        `);
-    }
+  }
 }
-
-
