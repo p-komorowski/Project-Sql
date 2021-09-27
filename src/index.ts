2dd238aee2d04f34aaf94f422d972ded4041f418
p-komorowski <pkomorowski29@gmail.com>
2021-09-27 15:31:12 +0200

    refactor(bootstrap): add cookie parser

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 import "reflect-metadata";
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { ValidationPipe } from "@nestjs/common";
-import * as cookieParser from "cookie-parser";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
       transformOptions: { enableImplicitConversion: false },
-    })
+    }),
   );
-  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
