6f8d98d6ca51cac9ed01db7b64125e695f160b45
p-komorowski <pkomorowski29@gmail.com>
2021-09-29 13:40:21 +0200

    refactor:(*): add relations

---
+++ src/modules/basket/entities/basket_books.entity.ts
@@ -0,0 +1,26 @@
+import { Books } from "src/modules/books/entity/books.entity";
+import { Entity, Column, OneToMany, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+import { Basket } from "./basket.entity";
+
+
+@Entity()
+export class BasketBooks {
+  @PrimaryColumn()
+  id: string = uuid();
+
+  @Column({ name: "basket_id" })
+  basketId: string = uuid();
+
+  @Column()
+  IBSN: string;
+
+  @Column()
+  quantity: number;
+
+  @OneToMany(() => Basket, (basket) => basket.basketId)
+  basket: Basket[];
+
+  @OneToMany(() => Books, (books) => books.IBSN)
+  books: Books[];
+}
