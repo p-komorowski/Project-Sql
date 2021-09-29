a65b29c38d68644bd90eac1ae3f6a8ea6d49a128
p-komorowski <pkomorowski29@gmail.com>
2021-09-29 13:38:39 +0200

    refactor(injectRepository): change name to BasketBooks

--- src/modules/basket/basket.service.ts
+++ src/modules/basket/basket.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { BasketDto } from "./dto/basket.dto";
-import { shoppingBasket } from "./entity/basket.entity";
+import { BasketBooks } from "./entities/basket_books.entity";
 import { BasketRepository } from "./repository/basket.repository";
 
 @Injectable()
 export class BasketService {
   constructor(
-    @InjectRepository(shoppingBasket)
+    @InjectRepository(BasketBooks)
     private readonly repository: BasketRepository
   ) {}
 
@@ -15,7 +15,7 @@ export class BasketService {
     return this.repository.save(newBasket);
   }
 
-  public async getProducts(): Promise<shoppingBasket[]> {
+  public async getProducts(): Promise<BasketBooks[]> {
     return this.repository.find();
   }
 
