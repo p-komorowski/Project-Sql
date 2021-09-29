ddd57f3fd6c39176502c0353b7e20ae3fbe96b0f
p-komorowski <pkomorowski29@gmail.com>
2021-09-29 13:39:24 +0200

    feat(*): add basketBooksDto class

---
+++ src/modules/basket/dto/basket_book.dto.ts
@@ -0,0 +1,17 @@
+import { Exclude, Expose } from "class-transformer";
+import { v4 as uuid } from "uuid";
+
+@Exclude()
+export class BasketBookDto {
+  @Expose()
+  id: string = uuid();
+
+  @Expose({ name: "basket_id"})
+  basketId: string = uuid();
+
+  @Expose()
+  IBSN: string;
+
+  @Expose()
+  quantity: number
+}
