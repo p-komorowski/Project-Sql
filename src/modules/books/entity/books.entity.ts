2779550a16761229ea8eefab915d4794cb94e2fd
p-komorowski <pkomorowski29@gmail.com>
2021-09-29 13:42:22 +0200

    refacotr(*): add/change relations

--- src/modules/books/entity/books.entity.ts
+++ src/modules/books/entity/books.entity.ts
@@ -1,4 +1,7 @@
-import { Entity, Column, PrimaryColumn } from "typeorm";
+import { BasketBooks } from "src/modules/basket/entities/basket_books.entity";
+import { OrderBooks } from "src/modules/order/dto/order_books.dto";
+import { Review } from "src/modules/review/dto/review.dto";
+import { Entity, Column, PrimaryColumn, ManyToOne, OneToMany } from "typeorm";
 
 @Entity()
 export class Books {
@@ -16,4 +19,13 @@ export class Books {
 
   @Column({ nullable: true })
   count: number;
+
+  @ManyToOne(() => BasketBooks, (basket) => basket.id)
+  basket: BasketBooks[];
+
+  @ManyToOne(() => OrderBooks, (orderBooks) => orderBooks.id)
+  orderBooks: OrderBooks[];
+
+  @OneToMany(() => Review, (review) => review.id)
+  review: Review[];
 }
