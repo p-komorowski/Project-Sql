9724a49f291c12a15461f645967a7788eb4e0f6a
p-komorowski <pkomorowski29@gmail.com>
2021-09-29 13:38:13 +0200

    initial commit

--- src/modules/basket/basket.module.ts
+++ src/modules/basket/basket.module.ts
@@ -2,11 +2,11 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { BasketController } from "./basket.controller";
 import { BasketService } from "./basket.service";
-import { shoppingBasket } from "./entity/basket.entity";
+import { BasketBooks } from "./entities/basket_books.entity";
 import { BasketRepository } from "./repository/basket.repository";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([shoppingBasket])],
+  imports: [TypeOrmModule.forFeature([BasketBooks])],
   providers: [BasketService, BasketRepository],
   controllers: [BasketController],
 })
