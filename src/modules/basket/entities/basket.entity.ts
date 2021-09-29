ee3df1c1f1cad797dbd68c161a51c3f7c69d365c
p-komorowski <pkomorowski29@gmail.com>
2021-09-29 13:40:31 +0200

    refactor:(*): add relations

---
+++ src/modules/basket/entities/basket.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, OneToMany, PrimaryColumn, ManyToOne } from "typeorm";
+import { User } from "../../user/entities/user.entity";
+import { v4 as uuid } from "uuid";
+import { BasketBooks } from "./basket_books.entity";
+
+@Entity()
+export class Basket {
+  @PrimaryColumn({ name: "basket_id" })
+  basketId: string = uuid();
+
+  @Column({ name: "user_id" })
+  userId: string = uuid();
+
+  @ManyToOne(() => User, (user) => user.id)
+  user: User[];
+
+  @ManyToOne(() => BasketBooks, (basket) => basket.id)
+  basket: BasketBooks[];
+}
