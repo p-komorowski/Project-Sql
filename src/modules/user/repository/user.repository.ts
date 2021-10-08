5434139518556870bd2d6290bc81a9066275a223
p-komorowski <pkomorowski29@gmail.com>
2021-10-08 12:31:45 +0200

    style(*):delete unnecessary comments, better looking code

--- src/modules/user/repository/user.repository.ts
+++ src/modules/user/repository/user.repository.ts
@@ -1,8 +1,5 @@
 import { EntityRepository, Repository } from "typeorm";
-import {User} from "../entities/user.entity";
+import { User } from "../entities/user.entity";
 
-// TODO
 @EntityRepository(User)
 export class userRepository extends Repository<User> {}
-
-// TODO: remove 'mongoose' import
