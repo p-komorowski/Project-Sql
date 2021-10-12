7a0525290b22b610e7b1db4b8fce5c07f5530599
p-komorowski <pkomorowski29@gmail.com>
2021-10-12 17:13:26 +0200

    [WIP]: add comments

--- src/migration/1633342748095-order.ts
+++ src/migration/1633342748095-order.ts
@@ -5,11 +5,11 @@ export class createTableOrder1633342748095 implements MigrationInterface {
     await queryRunner.query(`
             CREATE TABLE IF NOT EXISTS "order" ( 
                 order_id UUID, 
-                user_id UUID // dodać reference
+                user_id UUID 
                 )
         `);
   }
-
+// dodać reference
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
         DROP TABLE books
