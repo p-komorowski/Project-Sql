75981fbfbd7c3c341b9082b0fa78ec5284e48000
p-komorowski <pkomorowski29@gmail.com>
2021-10-12 17:19:22 +0200

    feat:add new functions

---
+++ src/modules/book/book.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@nestjs/common";
+import { BooksRepository } from "./repository/books.repository";
+import { Books } from "./entity/book.entity";
+import { Connection } from "typeorm";
+import { BookDto } from "./dto/book.dto";
+import { RequestContextProvider } from "../../middleware/request-context.middleware";
+
+@Injectable()
+export class BooksService {
+  private booksRepository: BooksRepository;
+  constructor(private readonly connection: Connection) {
+    this.booksRepository = this.connection.getCustomRepository(BooksRepository);
+  }
+
+  async insertProduct(newProduct: BookDto): Promise<Books> {
+    return this.booksRepository.save(newProduct);
+  }
+
+  public async getProducts(): Promise<Books[]> {
+    //const currentUser = RequestContextProvider.currentUser();
+    return this.booksRepository.find();
+  }
+
+   public async getBook(IBSN:string): Promise<Books> {
+     const test = await this.booksRepository.findOne({
+      where: {
+        IBSN:IBSN,
+      },
+    })
+    return test
+
+  }
+  async create(): Promise<Books> {
+    const book = new Books();
+    return this.booksRepository.save(book);
+  }
+  async deleteProduct(IBSN: string): Promise<void> {
+    await this.booksRepository.delete(IBSN);
+  }
+}
