5da4f6181642937c02ca26645b168aea77457ec7
p-komorowski <pkomorowski29@gmail.com>
2021-10-12 17:19:04 +0200

    change name

---
+++ src/modules/book/book.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { BooksService } from "./book.service";
+import { entities } from "../../config";
+import { BooksRepository } from "./repository/books.repository";
+import { UserModule } from "../user/user.module";
+import { BooksController } from "./book.controller";
+
+@Module({
+  imports: [TypeOrmModule.forFeature(entities), UserModule],
+  controllers: [BooksController],
+  providers: [BooksService, BooksRepository],
+})
+export class BooksModule {}
