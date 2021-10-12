6dcb5e5c3cbff97ddd2c43e9f907c5fc9a3c0883
p-komorowski <pkomorowski29@gmail.com>
2021-10-12 17:15:38 +0200

    add swagger components

--- src/modules/basket/dto/basket.dto.ts
+++ src/modules/basket/dto/basket.dto.ts
@@ -1,11 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Exclude, Expose } from "class-transformer";
 import { v4 as uuid } from "uuid";
 
 @Exclude()
 export class BasketDto {
   @Expose()
+  @ApiProperty({type: String, description: 'Basket_id',nullable:false})
   basketId: string = uuid();
 
   @Expose()
+  @ApiProperty({type: String, description: 'User_id',nullable:false})
   userId: string = uuid();
+
+  @Expose()
+  IBSN:string = uuid();
 }
--- src/modules/basket/dto/basket_book.dto.ts
+++ src/modules/basket/dto/basket_book.dto.ts
@@ -1,17 +1,22 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Exclude, Expose } from "class-transformer";
 import { v4 as uuid } from "uuid";
 
 @Exclude()
-export class BasketBooskDto {
+export class BasketBooksDto {
   @Expose()
+  @ApiProperty({type: String, description: 'id'})
   id: string = uuid();
 
   @Expose({ name: "basket_id" })
+  @ApiProperty({type: String, description: 'Basket id'})
   basketId: string = uuid();
 
   @Expose()
+  @ApiProperty({type: String, description: 'IBSN'})
   IBSN: string;
 
   @Expose()
+  @ApiProperty({type: String, description: 'Quantity'})
   quantity: number;
 }
