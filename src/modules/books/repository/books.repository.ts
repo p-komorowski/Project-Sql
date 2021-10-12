574c7625468a460627cb6252783b9c9dbb31c6b4
p-komorowski <84277719+p-komorowski@users.noreply.github.com>
2021-10-12 17:25:19 +0200

    Delete src/modules/books directory

--- src/modules/books/repository/books.repository.ts
+++
@@ -1,5 +0,0 @@
-import { Books } from "../entity/books.entity";
-import { EntityRepository, Repository } from "typeorm";
-
-@EntityRepository(Books)
-export class BooksRepository extends Repository<Books> {}
