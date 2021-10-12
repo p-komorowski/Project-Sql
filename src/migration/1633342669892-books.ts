bd3091698fc97b0575ca5bf658423ffb32997338
p-komorowski <pkomorowski29@gmail.com>
2021-10-12 17:12:10 +0200

    add foreign key referance

--- src/migration/1633342669892-books.ts
+++ src/migration/1633342669892-books.ts
@@ -4,7 +4,7 @@ export class createTableBooks1633342669892 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
     CREATE TABLE IF NOT EXISTS books (
-        IBSN serial PRIMARY KEY,
+        IBSN serial PRIMARY KEY FOREIGN KEY REFERENCES review(IBSN),
         title VARCHAR NOT NULL,
         price DECIMAL,
         author VARCHAR NOT NULL,
