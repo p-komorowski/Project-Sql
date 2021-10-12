97f527d614fa03a138a44260f2062c2de9890324
p-komorowski <pkomorowski29@gmail.com>
2021-10-12 17:21:24 +0200

    feat:add swagger

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,26 @@ import { ValidationPipe } from "@nestjs/common";
 import * as cookieParser from "cookie-parser";
 import { createConnection } from "typeorm";
 import { config, entities } from "./config";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const swagger = new DocumentBuilder()
+    .setTitle('Project-sql')
+    .setDescription('The Project-sql API description')
+    .setVersion('1.0')
+    .addTag('Endpoints')
+    .addBearerAuth(
+      {type:'http', scheme:'bearer', bearerFormat:'Token'},
+      'acces-token',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, swagger);
+  SwaggerModule.setup('api', app, document);
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
