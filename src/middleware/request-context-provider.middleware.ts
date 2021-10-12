fadc6386ff54aba66a0c14049f596b319b20550d
p-komorowski <pkomorowski29@gmail.com>
2021-10-12 17:10:25 +0200

    change name

---
+++ src/middleware/request-context-provider.middleware.ts
@@ -0,0 +1,36 @@
+import { NestMiddleware } from "@nestjs/common";
+import { Response, NextFunction } from "express";
+import { Token } from "../modules/auth/entity/token.entity";
+import { getManager } from "typeorm";
+import { getNamespace, createNamespace } from "cls-hooked";
+import { RequestContextProvider } from "./request-context.middleware";
+
+export class RequestContextMiddleware implements NestMiddleware {
+  async use(req: any, res: Response, next: NextFunction) {
+    let tokenHeader: string;
+    if (req.headers["authorization"]) {
+      tokenHeader = req.headers["authorization"].replace("Bearer ", "");
+    }
+
+    //upewnic sie ze token header nie jest pusty
+    const token = await getManager().findOne(Token, {
+      join: {
+        alias: "token",
+        leftJoinAndSelect: {
+          user: "token.user",
+        }
+      },
+      where: { token: tokenHeader },
+    });
+
+    req["User"] = token.user;
+    const requestContext = new RequestContextProvider(req, res);
+    const session =
+      getNamespace(RequestContextProvider.uuid) ||
+      createNamespace(RequestContextProvider.uuid);
+    session.run(async () => {
+      session.set(RequestContextProvider.name, requestContext);
+      next();
+    });
+  }
+}
