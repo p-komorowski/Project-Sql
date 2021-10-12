fb8b0bdc9d4a94cef556d03a478446fdb7c61910
p-komorowski <pkomorowski29@gmail.com>
2021-10-12 17:14:39 +0200

    add swagger components

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,7 +3,9 @@ import { AuthService } from "./auth.service";
 import { LoginDto } from "./dto/login.dto";
 import { RegisterDto } from "./dto/register.dto";
 import { Response } from "express";
+import { ApiBody, ApiOperation, ApiResponse, ApiTags, ApiUnauthorizedResponse } from "@nestjs/swagger";
 
+@ApiTags('Auth')
 @Controller()
 export class AuthController {
   logger: Logger;
@@ -12,12 +14,20 @@ export class AuthController {
   }
 
   @Post("auth/register")
+  @ApiOperation({ summary: 'Register User' })
+  @ApiResponse({ status: 201, description: 'User registration.' })
+  @ApiUnauthorizedResponse({ description: 'User already exists.'})
+  @ApiBody({type: RegisterDto})
   async register(@Body() reqisterDto: RegisterDto) {
     this.logger.warn("logger test warn");
     return this.authService.register(reqisterDto);
   }
 
   @Post("auth/login")
+  @ApiOperation({ summary: 'Log user' })
+  @ApiResponse({ status: 200, description: 'User Login.' })
+  @ApiUnauthorizedResponse({ description: 'Invalid credentials.'})
+  @ApiBody({type: LoginDto})
   async login(@Body() loginDto: LoginDto, @Res() response: Response) {
     const token = await this.authService.login(loginDto);
     const newTime = new Date();
