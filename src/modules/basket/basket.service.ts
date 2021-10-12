024a8859aa1efceab8bcdd89e8769272734832da
p-komorowski <pkomorowski29@gmail.com>
2021-10-12 17:17:34 +0200

    feat:add new functions

--- src/modules/basket/basket.service.ts
+++ src/modules/basket/basket.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { BooksService } from "../book/book.service";
+import { BookDto } from "../book/dto/book.dto";
+import { Books } from "../book/entity/book.entity";
+import { BooksRepository } from "../book/repository/books.repository";
 import { BasketDto } from "./dto/basket.dto";
-import { BasketBooks } from "./entities/basket_books.entity";
+import { BasketBooksDto } from "./dto/basket_book.dto";
+import { BasketBooks } from "./entities/basket_book.entity";
 import { BasketRepository } from "./repository/basket.repository";
+
 
 @Injectable()
 export class BasketService {
   constructor(
     @InjectRepository(BasketBooks)
-    private readonly repository: BasketRepository
+    private readonly repository: BasketRepository,
+    private readonly booksRepository: BooksRepository,
+    private readonly booksService:BooksService,
   ) {}
 
   async insertProduct(newBasket: BasketDto): Promise<any> {
@@ -21,5 +29,14 @@ export class BasketService {
 
   async deleteProduct(basket_id: string): Promise<void> {
     await this.repository.delete(basket_id);
+  }
+
+ async GetBookByIBSN(book:BasketBooksDto): Promise<Books>{
+   const test = await this.booksService.getBook(book.IBSN)
+   return test
+ }
+
+  async deleteBooks(IBSN: string): Promise<void> {
+    await this.repository.delete(IBSN);
   }
 }
