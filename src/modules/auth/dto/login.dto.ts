6095a4c36c94db55d261fed0c63d8cc8d0c5b8e3
p-komorowski <pkomorowski29@gmail.com>
2021-10-12 17:14:00 +0200

    add ApiProperty

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Exclude, Expose } from "class-transformer";
 import { IsEmail, IsString } from "class-validator";
 
@@ -6,9 +7,11 @@ export class LoginDto {
   @Expose()
   @IsEmail()
   @IsString()
+  @ApiProperty({type: String, description: 'email'})
   email: string;
 
   @Expose()
   @IsString()
+  @ApiProperty({type: String, description: 'password'})
   password: string;
 }
--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Exclude, Expose } from "class-transformer";
 import { IsEmail, IsString } from "class-validator";
 
@@ -5,13 +6,16 @@ import { IsEmail, IsString } from "class-validator";
 export class RegisterDto {
   @Expose()
   @IsEmail()
+  @ApiProperty({type: String, description: 'email'})
   email: string;
 
   @Expose()
   @IsString()
+  @ApiProperty({type: String, description: 'password'})
   password: string;
 
   @Expose()
   @IsString()
+  @ApiProperty({type: String, description: 'name'})
   name: string;
 }
