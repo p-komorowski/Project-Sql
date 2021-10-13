4442fe8282a421a2235c6929137d93a12688aeaa
p-komorowski <pkomorowski29@gmail.com>
2021-10-13 13:36:52 +0200

    [WIP]:working on GetBookByIBSN and create method

--- src/modules/basket/basket.service.ts
+++ src/modules/basket/basket.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { BooksService } from "../book/book.service";
 import { BookDto } from "../book/dto/book.dto";
@@ -6,6 +6,7 @@ import { Books } from "../book/entity/book.entity";
 import { BooksRepository } from "../book/repository/books.repository";
 import { BasketDto } from "./dto/basket.dto";
 import { BasketBooksDto } from "./dto/basket_book.dto";
+import { Basket } from "./entities/basket.entity";
 import { BasketBooks } from "./entities/basket_book.entity";
 import { BasketRepository } from "./repository/basket.repository";
 
@@ -16,7 +17,7 @@ export class BasketService {
     @InjectRepository(BasketBooks)
     private readonly repository: BasketRepository,
     private readonly booksRepository: BooksRepository,
-    private readonly booksService:BooksService,
+    private readonly booksService:BooksService
   ) {}
 
   async insertProduct(newBasket: BasketDto): Promise<any> {
@@ -31,10 +32,22 @@ export class BasketService {
     await this.repository.delete(basket_id);
   }
 
- async GetBookByIBSN(book:BasketBooksDto): Promise<Books>{
+ async GetBookByIBSN(book:BookDto): Promise<BasketBooksDto>{
    const test = await this.booksService.getBook(book.IBSN)
-   return test
+   if(test){
+   const addedBook = await this.repository.create(book)
+   return addedBook
+  }
+  else {
+    throw new UnauthorizedException("no book in stock")
+  }
  }
+
+ async create(addedBook: BookDto): Promise<Books> {
+  const book = new Books(addedBook);
+  return this.repository.save(book);
+}
+
 
   async deleteBooks(IBSN: string): Promise<void> {
     await this.repository.delete(IBSN);
