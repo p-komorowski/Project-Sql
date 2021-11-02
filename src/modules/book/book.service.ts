4bf140c69a583e6f8a755ba84255ea0a85e5798e
p-komorowski <pkomorowski29@gmail.com>
2021-11-02 14:07:39 +0100

    feat(*): add new methods

--- src/modules/book/book.service.ts
+++ src/modules/book/book.service.ts
@@ -1,42 +1,75 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { BooksRepository } from "./repository/books.repository";
-import { Books } from "./entity/book.entity";
+import { Book } from "./entity/book.entity";
 import { Connection } from "typeorm";
 import { BookDto } from "./dto/book.dto";
-import { RequestContextProvider } from "../../middleware/request-context.middleware";
+import { ReviewRepository } from "../review/repository/review.repository";
+import { ReviewDto } from "../review/dto/review.dto";
 
 @Injectable()
 export class BooksService {
   private booksRepository: BooksRepository;
+  private reviewRepository: ReviewRepository;
   constructor(private readonly connection: Connection) {
     this.booksRepository = this.connection.getCustomRepository(BooksRepository);
+    this.reviewRepository =
+      this.connection.getCustomRepository(ReviewRepository);
   }
 
-  async insertProduct(newProduct: BookDto): Promise<Books> {
+  async insertProduct(newProduct: BookDto): Promise<Book> {
     return this.booksRepository.save(newProduct);
   }
 
-  public async getProducts(): Promise<Books[]> {
-    //const currentUser = RequestContextProvider.currentUser();
-    return this.booksRepository.find();
+  public async getProducts(): Promise<Book[]> {
+    return this.booksRepository.find({
+      relations: ["review"],
+    });
   }
 
-   public async getBook(IBSN:string): Promise<Books> {
-     const test = await this.booksRepository.findOne({
+  public async getBook(IBSN: string): Promise<Book> {
+    const book = await this.booksRepository.findOne({
+      relations: ["review"],
       where: {
-        IBSN:IBSN,
+        IBSN: IBSN,
       },
-    })
-    if(!test){
-      throw new UnauthorizedException("cannot find book")
+    });
+    if (!book) {
+      throw new UnauthorizedException("cannot find book");
+    } else {
+      return book;
     }
-    else {
-      return test
+  }
+
+  async addReviewToBook(dto: ReviewDto) {
+    const book = await this.getBook(dto.IBSN);
+    if (!book) {
+      throw new UnauthorizedException("book does not exist");
     }
 
+    const newReview = await this.reviewRepository.create({
+      review: dto.review,
+    });
+    newReview.book = book;
+
+    return await this.reviewRepository.save(newReview);
   }
-  
+
+  async changePriceOfBook(IBSN: string, price: number) {
+    const book = await this.getBook(IBSN);
+    if (!book) {
+      throw new UnauthorizedException("book does not exist");
+    }
+    book.price = price;
+
+    return await this.booksRepository.save(book);
+  }
+
   async deleteProduct(IBSN: string): Promise<void> {
     await this.booksRepository.delete(IBSN);
   }
+
+  async deleteReview(id: string): Promise<void> {
+    await this.reviewRepository.delete(id);
+  }
+  
 }
