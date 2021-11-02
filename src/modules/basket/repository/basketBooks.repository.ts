a37dffc1eb2b803e34e9b45413c39fc28cb3a1b8
p-komorowski <pkomorowski29@gmail.com>
2021-11-02 14:00:24 +0100

    style:better looking code

--- src/modules/basket/repository/basketBooks.repository.ts
+++ src/modules/basket/repository/basketBooks.repository.ts
@@ -1,8 +1,5 @@
-
 import { EntityRepository, Repository } from "typeorm";
 import { Basket } from "../entities/basket.entity";
-import { BasketBooks } from "../entities/basket_book.entity";
-
 
 @EntityRepository(Basket)
 export class BasketRepository extends Repository<Basket> {}
