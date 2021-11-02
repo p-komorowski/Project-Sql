57e9ee91899fad072862cfa7fed98648e15ac4ef
p-komorowski <pkomorowski29@gmail.com>
2021-11-02 14:02:04 +0100

    style:better looking code

--- src/modules/basket/basket.module.ts
+++ src/modules/basket/basket.module.ts
@@ -1,21 +1,23 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { BooksService } from "../book/book.service";
-import { Books } from "../book/entity/book.entity";
+import { Book } from "../book/entity/book.entity";
 import { BooksRepository } from "../book/repository/books.repository";
-import { userRepository } from "../user/repository/user.repository";
+import { OrderService } from "../order/order.service";
+import { OrderRepository } from "../order/repository/order.repository";
+import { UserRepository } from "../user/repository/user.repository";
 import { UsersService } from "../user/user.service";
 import { BasketController } from "./basket.controller";
 import { BasketService } from "./basket.service";
 import { Basket } from "./entities/basket.entity";
-import { BasketBooks } from "./entities/basket_book.entity";
+import { BasketBook } from "./entities/basket_book.entity";
 import { BasketBooksRepository } from "./repository/basket.repository";
 import { BasketRepository } from "./repository/basketBooks.repository";
 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BasketBooks,Basket,Books,BasketBooksRepository,BasketRepository,userRepository])],
-  providers: [BasketService, BasketRepository,BooksRepository,BooksService,BasketBooksRepository,UsersService],
+  imports: [TypeOrmModule.forFeature([BasketBook,Basket,Book,BasketBooksRepository,BasketRepository,UserRepository])],
+  providers: [BasketService, BasketRepository,BooksRepository,BooksService,BasketBooksRepository,UsersService,OrderService,OrderRepository],
   controllers: [BasketController],
 })
 export class BasketModule {}
