44d976554ff208c6eec873b09a93cad706d37fe5
p-komorowski <pkomorowski29@gmail.com>
2021-11-02 13:58:18 +0100

    refactor(*):change relations

--- src/modules/basket/entities/basket_book.entity.ts
+++ src/modules/basket/entities/basket_book.entity.ts
@@ -1,24 +1,9 @@
-import { Books } from "../../book/entity/book.entity";
-import {
-  Entity,
-  Column,
-  PrimaryColumn,
-  DeepPartial,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-  ManyToMany,
-  OneToOne,
-} from "typeorm";
+import { Book } from "../../book/entity/book.entity";
+import {Entity, Column, ManyToOne, PrimaryGeneratedColumn, JoinColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
 import { Basket } from "./basket.entity";
-import { ApiProperty } from "@nestjs/swagger";
-import { BasketBooksInterface } from "../interface/basket_books.interface";
-
 @Entity()
-export class BasketBooks {
-  // constructor(basket: DeepPartial<BasketBooksInterface> ){
-  //   Object.assign(this, basket);
-  // }
+export class BasketBook {
 
   @PrimaryGeneratedColumn("uuid")
   id: string = uuid();
@@ -26,8 +11,9 @@ export class BasketBooks {
   @Column()
   count: number;
 
-  @OneToOne(() => Books, (books) => books.basketBooks)
-  books: Books;
+  @ManyToOne(() => Book, (book) => book.basketBook)
+  @JoinColumn()
+  book: Book;
 
   @ManyToOne(() => Basket, (basket) => basket.basketBooks)
   basket: Basket;
