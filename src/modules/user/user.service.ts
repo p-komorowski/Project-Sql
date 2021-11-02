2baf8a4cb05a87c694b47429a629042a080d03cd
p-komorowski <pkomorowski29@gmail.com>
2021-11-02 14:13:27 +0100

    refactor: delete comments, add new methods

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,24 +1,22 @@
 import { Injectable } from "@nestjs/common";
 import { Connection } from "typeorm";
-import { User } from "./entities";
+import { Customer } from "./entities";
 import { RegisterDto } from "../auth/dto/register.dto";
-import { userRepository } from "./repository/user.repository";
-import { RequestContextProvider } from "src/middleware/request-context.middleware";
-import { OrderBooks } from "../order/dto/order_books.dto";
+import { UserRepository } from "./repository/user.repository";
 import { LoginDto } from "../auth/dto/login.dto";
 
 @Injectable()
 export class UsersService {
-  private repository: userRepository;
+  private repository: UserRepository;
   constructor(private readonly connection: Connection) {
-    this.repository = this.connection.getCustomRepository(userRepository);
+    this.repository = this.connection.getCustomRepository(UserRepository);
   }
 
-  findAll(): Promise<User[]> {
+  findAll(): Promise<Customer[]> {
     return this.repository.find();
   }
 
-  async findByEmail(email: string): Promise<User> {
+  async findByEmail(email: string): Promise<Customer> {
     return this.repository.findOne({
       where: {
         email: email,
@@ -26,26 +24,13 @@ export class UsersService {
     });
   }
 
-  async findById(id: LoginDto): Promise<User> {
+  async findById(id: LoginDto): Promise<Customer> {
     return this.repository.findOne(id);
   }
 
-
-  // async findOrder(orderId:string []){
-
-  //    const order = this.orderService.getAll(orderId)
-  //   const user = RequestContextProvider.currentUser();
-
-  //   // user.order=OrderBooks;
-  //   // await this.repository.update(user.id,user)
-  //   await this.repository.save({
-  //     ...user,
-  //     order:order
-  //   })
-  // }
-
-  async create(newUser: RegisterDto): Promise<User> {
-    const userEntity = new User(newUser);
+  async create(newUser: RegisterDto): Promise<Customer> {
+    const userEntity = new Customer(newUser);
     return this.repository.save(userEntity);
   }
+  
 }
