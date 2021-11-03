7d820c8aa626cfd789c4ada733fdd8483ebbf731
p-komorowski <pkomorowski29@gmail.com>
2021-11-03 12:59:08 +0100

    feat(orderController): add roles

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -1,6 +1,10 @@
-import { Controller, Post, UseGuards } from "@nestjs/common";
+import { Controller, Get, Post, UseGuards } from "@nestjs/common";
 import {ApiBearerAuth, ApiOperation, ApiResponse, ApiTags, ApiUnauthorizedResponse } from "@nestjs/swagger";
+import { Roles } from "../auth/decorators/role.decorator";
 import { JwtAuthGuard } from "../auth/strategy/jwt-auth.guard";
+import { Role } from "../auth/strategy/models/role.enum";
+import { RolesGuard } from "../auth/strategy/roles.guard";
+import { Order } from "./entity/order.entity";
 import { OrderService } from "./order.service";
 
 @ApiTags("Order")
@@ -8,8 +12,10 @@ import { OrderService } from "./order.service";
 export class OrderController {
   constructor(private orderService: OrderService) {}
 
-  @UseGuards(JwtAuthGuard)
+  
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Post()
+  @Roles(Role.User)
   @ApiBearerAuth()
   @ApiOperation({ summary: 'Create order for logged yser' })
   @ApiResponse({ status: 200, description: 'Order created.' })
@@ -17,4 +23,17 @@ export class OrderController {
   async addBasketToOrder() {
     await this.orderService.createOrder();
   }
+
+  
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Get()
+  @Roles(Role.User)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Show list of places orders' })
+  @ApiResponse({ status: 200, description: 'Order list shown' })
+  @ApiUnauthorizedResponse({ description: 'User not logged in.' })
+  async getAllOrders():Promise<Order[]>{
+   return await this.orderService.getAllOrders()
+  }
+  
 }
