4289a581cec0eda2d21701365e891604e75433af
p-komorowski <pkomorowski29@gmail.com>
2021-11-03 12:59:30 +0100

    refactor: add imports

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -7,13 +7,12 @@ import { BasketBooksRepository } from "../basket/repository/basket.repository";
 import { BasketRepository } from "../basket/repository/basketBooks.repository";
 import { BooksService } from "../book/book.service";
 import { Book } from "../book/entity/book.entity";
-import { BooksRepository } from "../book/repository/books.repository";
+import { BooksRepository } from "../book/repository/book.repository";
 import { UserRepository } from "../user/repository/user.repository";
 import { UsersService } from "../user/user.service";
 import { OrderController } from "./order.controller";
 import { OrderService } from "./order.service";
 import { OrderRepository } from "./repository/order.repository";
-
 
 @Module({
     imports: [TypeOrmModule.forFeature([BasketBook,Basket,Book,BasketBooksRepository,BasketRepository,UserRepository])],
