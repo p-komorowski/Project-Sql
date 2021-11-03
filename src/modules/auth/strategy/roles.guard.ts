6a8ff87426b16dbca69bba11944291665d49f827
p-komorowski <pkomorowski29@gmail.com>
2021-11-03 12:52:29 +0100

    refactor(RolesGuard): change spelling from roles to role in line 20

--- src/modules/auth/strategy/roles.guard.ts
+++ src/modules/auth/strategy/roles.guard.ts
@@ -8,6 +8,7 @@ export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
+
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
@@ -15,7 +16,8 @@ export class RolesGuard implements CanActivate {
     if (!requiredRoles) {
       return true;
     }
-    const { user } = context.switchToHttp().getRequest();
-    return requiredRoles.some((role) => user.roles?.includes(role));
+    const user = context.switchToHttp().getRequest().User;
+    return requiredRoles.some((role) => user.role?.includes(role));
+  
   }
 }
