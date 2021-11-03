aa97d5a72cae400603640be13354a130de33a407
p-komorowski <pkomorowski29@gmail.com>
2021-11-03 13:02:17 +0100

    refactor: add rolesGuard to provider and exclude paths for middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { config } from "./config";
@@ -13,16 +13,36 @@ import { UserModule } from "./modules/user/user.module";
 import { BasketController } from "./modules/basket/basket.controller";
 import { OrderModule } from "./modules/order/order.module";
 import { OrderController } from "./modules/order/order.controller";
+import { APP_GUARD } from "@nestjs/core";
+import { RolesGuard } from "./modules/auth/strategy/roles.guard";
 
 @Module({
-  imports: [ TypeOrmModule.forRoot(config.database), AuthModule, UserModule, BooksModule, BasketModule, OrderModule ],
+  imports: [
+    TypeOrmModule.forRoot(config.database),
+    AuthModule,
+    UserModule,
+    BooksModule,
+    BasketModule,
+    OrderModule,
+  ],
   controllers: [AppController],
-  providers: [AuthRepository, RequestContextProvider],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+    AuthRepository,
+    RequestContextProvider,
+  ],
 })
+
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(RequestContextMiddleware)
+      .exclude({path: 'books', method:RequestMethod.GET},
+      {path:'books/review', method: RequestMethod.POST}
+      )
       .forRoutes(BooksController, BasketController, OrderController);
   }
 }
