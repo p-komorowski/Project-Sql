ebbc69a6bff2b203443907633f3c673ef3f96f9f
p-komorowski <pkomorowski29@gmail.com>
2021-11-08 14:47:57 +0100

    style: better looking code

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,41 +1,39 @@
-import { Module } from "@nestjs/common";
-import { JwtModule } from "@nestjs/jwt";
-import { PassportModule } from "@nestjs/passport";
-import { AuthService } from "./auth.service";
-import { JwtStrategy } from "./strategy/jwt.strategy";
-import { LocalStrategy } from "./strategy/local.strategy";
-import { UserModule } from "../user/user.module";
-import { AuthController } from "./auth.controller";
-import { AuthRepository } from "./repository/auth.repository";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { AuthStrategy } from "./strategy/auth.strategy";
-import { RequestContextProvider } from "../../middleware/request-context.middleware";
-import { UsersService } from "../user/user.service";
-import { RolesGuard } from "./strategy/roles.guard";
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { UserModule } from '../user/user.module';
+import { AuthController } from './auth.controller';
+import { AuthRepository } from './repository/auth.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthStrategy } from './strategy/auth.strategy';
+import { RequestContextProvider } from '../../middleware/request-context.middleware';
+import { UsersService } from '../user/user.service';
+import { RolesGuard } from './guards/roles.guard';
 
 @Module({
-  imports: [
-    TypeOrmModule,
-    AuthModule,
-    UserModule,
-    PassportModule,
-    RequestContextProvider,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: "600s" },
-    }),
-  ],
-  controllers: [AuthController],
-  providers: [
-    UsersService,
-    AuthService,
-    LocalStrategy,
-    JwtStrategy,
-    AuthRepository,
-    AuthStrategy,
-    RequestContextProvider,
-    RolesGuard
-  ],
-  exports: [AuthService, JwtStrategy],
+    imports: [
+        TypeOrmModule,
+        AuthModule,
+        UserModule,
+        PassportModule,
+        RequestContextProvider,
+        JwtModule.register({
+            secret: process.env.JWT_SECRET,
+            signOptions: { expiresIn: '600s' },
+        }),
+    ],
+    controllers: [AuthController],
+    providers: [
+        UsersService,
+        AuthService,
+        JwtStrategy,
+        AuthRepository,
+        AuthStrategy,
+        RequestContextProvider,
+        RolesGuard,
+    ],
+    exports: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
