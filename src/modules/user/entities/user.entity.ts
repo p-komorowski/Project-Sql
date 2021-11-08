233f13e46257168822fc58ae44c970e35ff686dd
p-komorowski <pkomorowski29@gmail.com>
2021-11-08 15:03:17 +0100

    style: better looking code

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,45 +1,52 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany, OneToOne, DeepPartial, JoinColumn } from "typeorm";
-import { ContactDetails } from "./contact-details.entity";
-import { Token } from "../../auth/entity/token.entity";
-import { v4 as uuid } from "uuid";
-import { UserInterface } from "../interface/user.interface";
-import { Basket } from "../../basket/entities/basket.entity";
-import { Order } from "../../order/entity/order.entity";
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    OneToMany,
+    OneToOne,
+    DeepPartial,
+    JoinColumn,
+} from 'typeorm';
+import { ContactDetails } from './contact-details.entity';
+import { Token } from '../../auth/entity/token.entity';
+import { v4 as uuid } from 'uuid';
+import { UserInterface } from '../interface/user.interface';
+import { Basket } from '../../basket/entities/basket.entity';
+import { Order } from '../../order/entity/order.entity';
 
-@Entity("customer")
+@Entity('customer')
 export class Customer {
-  constructor(customer: DeepPartial<UserInterface>) {
-    Object.assign(this, customer);
-  }
+    constructor(customer: DeepPartial<UserInterface>) {
+        Object.assign(this, customer);
+    }
 
-  @PrimaryGeneratedColumn("uuid")
-  id: string = uuid();
+    @PrimaryGeneratedColumn('uuid')
+    id: string = uuid();
 
-  @Column()
-  email: string;
+    @Column()
+    email: string;
 
-  @Column()
-  name: string;
+    @Column()
+    name: string;
 
-  @Column()
-  password: string;
+    @Column()
+    password: string;
 
-  @Column({nullable:true})
-  role:string;
+    @Column({ nullable: true })
+    role: string;
 
-  @OneToMany(() => Token, (token) => token.id)
-  token: Token[];
+    @OneToMany(() => Token, (token) => token.id)
+    token: Token[];
 
-  @OneToOne(() => Basket, (basket) => basket.customer, { cascade: true })
-  @JoinColumn()
-  basket: Basket;
+    @OneToOne(() => Basket, (basket) => basket.customer, { cascade: true })
+    @JoinColumn()
+    basket: Basket;
 
-  @OneToOne(() => Order, (order) => order.user)
-  @JoinColumn()
-  order: Order;
+    @OneToOne(() => Order, (order) => order.user)
+    @JoinColumn()
+    order: Order;
 
-  @OneToOne(() => ContactDetails, )
-  @JoinColumn()
-  contactDetails: ContactDetails[];
-  
+    @OneToOne(() => ContactDetails)
+    @JoinColumn()
+    contactDetails: ContactDetails[];
 }
