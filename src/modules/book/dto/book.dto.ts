3f366d0309ddfa9d2ecc9e852c5859e3b6fe3988
p-komorowski <pkomorowski29@gmail.com>
2021-11-08 14:55:02 +0100

    style: better looking code

--- src/modules/book/dto/book.dto.ts
+++ src/modules/book/dto/book.dto.ts
@@ -1,32 +1,35 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { Exclude, Expose } from "class-transformer";
-import { IsNumber, IsString } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { Exclude, Expose } from 'class-transformer';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 @Exclude()
 export class BookDto {
-  @Expose()
-  @IsNumber()
-  @ApiProperty({ type: Number, description: "IBSN" })
-  IBSN: string;
+    @Expose()
+    @IsNumber()
+    @ApiProperty({ type: Number, description: 'IBSN' })
+    IBSN: string;
 
-  @Expose()
-  @IsString()
-  @ApiProperty({ description: "Title of book" })
-  title: string;
+    @Expose()
+    @IsString()
+    @ApiProperty({ description: 'Title of book' })
+    @IsOptional()
+    title: string;
 
-  @Expose()
-  @IsNumber()
-  @ApiProperty({ description: "price of book" })
-  price: number;
+    @Expose()
+    @IsNumber()
+    @ApiProperty({ description: 'price of book' })
+    @IsOptional()
+    price: number;
 
-  @Expose()
-  @IsString()
-  @ApiProperty({ description: "Name of author" })
-  author: string;
+    @Expose()
+    @IsString()
+    @ApiProperty({ description: 'Name of author' })
+    @IsOptional()
+    author: string;
 
-  @Expose()
-  @IsNumber()
-  @ApiProperty({ description: "count of book" })
-  count: number;
-
+    @Expose()
+    @IsNumber()
+    @ApiProperty({ description: 'count of book' })
+    @IsOptional()
+    count: number;
 }
