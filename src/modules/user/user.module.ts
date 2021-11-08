53cc0bd9e846a396d23d88eeb16a172597317470
p-komorowski <pkomorowski29@gmail.com>
2021-11-08 15:04:48 +0100

    style: better looking code

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,12 +1,12 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Customer } from "./entities/user.entity";
-import { UserRepository } from "./repository/user.repository";
-import { UsersService } from "./user.service";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Customer } from './entities/user.entity';
+import { UserRepository } from './repository/user.repository';
+import { UsersService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Customer])],
-  providers: [UsersService, UserRepository],
-  exports: [UsersService],
+    imports: [TypeOrmModule.forFeature([Customer])],
+    providers: [UsersService, UserRepository],
+    exports: [UsersService],
 })
 export class UserModule {}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,36 +1,35 @@
-import { Injectable } from "@nestjs/common";
-import { Connection } from "typeorm";
-import { Customer } from "./entities";
-import { RegisterDto } from "../auth/dto/register.dto";
-import { UserRepository } from "./repository/user.repository";
-import { LoginDto } from "../auth/dto/login.dto";
+import { Injectable } from '@nestjs/common';
+import { Connection } from 'typeorm';
+import { Customer } from './entities';
+import { RegisterDto } from '../auth/dto/register.dto';
+import { UserRepository } from './repository/user.repository';
+import { LoginDto } from '../auth/dto/login.dto';
 
 @Injectable()
 export class UsersService {
-  private repository: UserRepository;
-  constructor(private readonly connection: Connection) {
-    this.repository = this.connection.getCustomRepository(UserRepository);
-  }
+    private repository: UserRepository;
+    constructor(private readonly connection: Connection) {
+        this.repository = this.connection.getCustomRepository(UserRepository);
+    }
 
-  findAll(): Promise<Customer[]> {
-    return this.repository.find();
-  }
+    findAll(): Promise<Customer[]> {
+        return this.repository.find();
+    }
 
-  async findByEmail(email: string): Promise<Customer> {
-    return this.repository.findOne({
-      where: {
-        email: email,
-      },
-    });
-  }
+    async findByEmail(email: string): Promise<Customer> {
+        return this.repository.findOne({
+            where: {
+                email: email,
+            },
+        });
+    }
 
-  async findById(id: LoginDto): Promise<Customer> {
-    return this.repository.findOne(id);
-  }
+    async findById(id: LoginDto): Promise<Customer> {
+        return this.repository.findOne(id);
+    }
 
-  async create(newUser: RegisterDto): Promise<Customer> {
-    const userEntity = new Customer(newUser);
-    return this.repository.save(userEntity);
-  }
-  
+    async create(newUser: RegisterDto): Promise<Customer> {
+        const userEntity = new Customer(newUser);
+        return this.repository.save(userEntity);
+    }
 }
