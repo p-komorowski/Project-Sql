d0ea6ad70a7c17c0ced56cf244c06b20de467291
p-komorowski <pkomorowski29@gmail.com>
2021-11-08 14:57:23 +0100

    style: better looking code

--- src/modules/book/book.module.ts
+++ src/modules/book/book.module.ts
@@ -1,14 +1,14 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { BooksService } from "./book.service";
-import { entities } from "../../config";
-import { BooksRepository } from "./repository/book.repository";
-import { UserModule } from "../user/user.module";
-import { BooksController } from "./book.controller";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BooksService } from './book.service';
+import { entities } from '../../config';
+import { BooksRepository } from './repository/book.repository';
+import { UserModule } from '../user/user.module';
+import { BooksController } from './book.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature(entities), UserModule],
-  controllers: [BooksController],
-  providers: [BooksService, BooksRepository],
+    imports: [TypeOrmModule.forFeature(entities), UserModule],
+    controllers: [BooksController],
+    providers: [BooksService, BooksRepository],
 })
 export class BooksModule {}
