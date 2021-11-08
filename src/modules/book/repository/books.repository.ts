53adab8c542ffc203e71e2e29f2b47c1a6470543
p-komorowski <pkomorowski29@gmail.com>
2021-11-08 14:56:15 +0100

    style: better looking code

--- src/modules/book/repository/books.repository.ts
+++
@@ -1,5 +0,0 @@
-import { Book } from "../entity/book.entity";
-import { EntityRepository, Repository } from "typeorm";
-
-@EntityRepository(Book)
-export class BooksRepository extends Repository<Book> {}
