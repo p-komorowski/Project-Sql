086e87c8c7d04f06c3fefb1884908034bbeef8a5
p-komorowski <pkomorowski29@gmail.com>
2021-11-08 14:46:33 +0100

    style: better looking code

--- src/modules/auth/entity/token.entity.ts
+++ src/modules/auth/entity/token.entity.ts
@@ -1,27 +1,27 @@
-import { Customer } from "../../user/entities";
-import { Entity, Column, PrimaryColumn, ManyToOne, JoinColumn, DeepPartial } from "typeorm";
-import { v4 as uuid } from "uuid";
-import { TokenInterface } from "../interface/token.interface";
+import { Customer } from '../../user/entities';
+import { Entity, Column, PrimaryColumn, ManyToOne, JoinColumn, DeepPartial } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { TokenInterface } from '../interface/token.interface';
 
 @Entity()
 export class Token {
-  constructor(token: DeepPartial<TokenInterface>) {
-    Object.assign(this, token);
-  }
+    constructor(token: DeepPartial<TokenInterface>) {
+        Object.assign(this, token);
+    }
 
-  @PrimaryColumn()
-  id: string = uuid();
+    @PrimaryColumn()
+    id: string = uuid();
 
-  @ManyToOne(() => Customer, (user) => user.id)
-  @JoinColumn({ name: "user_id" })
-  user: Customer;
+    @ManyToOne(() => Customer, (user) => user.id)
+    @JoinColumn({ name: 'user_id' })
+    user: Customer;
 
-  @Column()
-  token: string;
+    @Column()
+    token: string;
 
-  @Column({ name: "exp_time" })
-  expTime: Date;
+    @Column({ name: 'exp_time' })
+    expTime: Date;
 
-  @Column({ name: "last_login" })
-  lastLogin: Date;
+    @Column({ name: 'last_login' })
+    lastLogin: Date;
 }
