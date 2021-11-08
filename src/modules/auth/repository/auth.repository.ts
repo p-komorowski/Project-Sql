4e2c9ed0e9099b36034acdfdc0b39687fb5ddaea
p-komorowski <pkomorowski29@gmail.com>
2021-11-08 14:47:21 +0100

    style: better looking code

--- src/modules/auth/repository/auth.repository.ts
+++ src/modules/auth/repository/auth.repository.ts
@@ -1,5 +1,5 @@
-import { EntityRepository, Repository } from "typeorm";
-import { Token } from "../entity/token.entity";
+import { EntityRepository, Repository } from 'typeorm';
+import { Token } from '../entity/token.entity';
 
 @EntityRepository(Token)
 export class AuthRepository extends Repository<Token> {}
