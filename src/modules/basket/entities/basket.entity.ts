466286345101d9d528e8a214d6b21822883804d2
p-komorowski <pkomorowski29@gmail.com>
2021-11-08 14:50:47 +0100

    style:better looking code

--- src/modules/basket/entities/basket.entity.ts
+++ src/modules/basket/entities/basket.entity.ts
@@ -1,22 +1,28 @@
-import { Entity, PrimaryColumn, ManyToOne, JoinTable, OneToOne, OneToMany } from "typeorm";
-import { Customer } from "../../user/entities/user.entity";
-import { v4 as uuid } from "uuid";
-import { BasketBook } from "./basket_book.entity";
-import { Order } from "../../order/entity/order.entity";
+import {
+    Entity,
+    PrimaryColumn,
+    ManyToOne,
+    JoinTable,
+    OneToOne,
+    OneToMany,
+} from 'typeorm';
+import { Customer } from '../../user/entities/user.entity';
+import { v4 as uuid } from 'uuid';
+import { BasketBook } from './basket-book.entity';
+import { Order } from '../../order/entity/order.entity';
 
 @Entity()
 export class Basket {
-  @PrimaryColumn()
-  id: string = uuid();
+    @PrimaryColumn()
+    id: string = uuid();
 
-  @OneToOne(() => Customer, (customer) => customer.basket)
-  customer: Customer;
+    @OneToOne(() => Customer, (customer) => customer.basket)
+    customer: Customer;
 
-  @OneToMany(() => BasketBook, (basketBooks) => basketBooks.basket)
-  @JoinTable()
-  basketBooks:BasketBook[];
+    @OneToMany(() => BasketBook, (basketBooks) => basketBooks.basket)
+    @JoinTable()
+    basketBooks: BasketBook[];
 
-  @OneToOne(() => Order, (order) => order.basket)
-  order: Order;
-
+    @OneToOne(() => Order, (order) => order.basket)
+    order: Order;
 }
