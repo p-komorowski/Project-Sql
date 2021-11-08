aa45672c698eb22bb87adcd899175bdc01a41b9e
p-komorowski <pkomorowski29@gmail.com>
2021-11-08 15:05:33 +0100

    style: better looking code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,48 +1,48 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { AppController } from "./app.controller";
-import { config } from "./config";
-import { RequestContextProvider } from "./middleware/request-context.middleware";
-import { RequestContextMiddleware } from "./middleware/request-context-provider.middleware";
-import { AuthModule } from "./modules/auth/auth.module";
-import { AuthRepository } from "./modules/auth/repository/auth.repository";
-import { BasketModule } from "./modules/basket/basket.module";
-import { BooksController } from "./modules/book/book.controller";
-import { BooksModule } from "./modules/book/book.module";
-import { UserModule } from "./modules/user/user.module";
-import { BasketController } from "./modules/basket/basket.controller";
-import { OrderModule } from "./modules/order/order.module";
-import { OrderController } from "./modules/order/order.controller";
-import { APP_GUARD } from "@nestjs/core";
-import { RolesGuard } from "./modules/auth/strategy/roles.guard";
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { config } from './config';
+import { RequestContextProvider } from './middleware/request-context.middleware';
+import { RequestContextMiddleware } from './middleware/request-context-provider.middleware';
+import { AuthModule } from './modules/auth/auth.module';
+import { AuthRepository } from './modules/auth/repository/auth.repository';
+import { BasketModule } from './modules/basket/basket.module';
+import { BooksController } from './modules/book/book.controller';
+import { BooksModule } from './modules/book/book.module';
+import { UserModule } from './modules/user/user.module';
+import { BasketController } from './modules/basket/basket.controller';
+import { OrderModule } from './modules/order/order.module';
+import { OrderController } from './modules/order/order.controller';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './modules/auth/guards/roles.guard';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(config.database),
-    AuthModule,
-    UserModule,
-    BooksModule,
-    BasketModule,
-    OrderModule,
-  ],
-  controllers: [AppController],
-  providers: [
-    {
-      provide: APP_GUARD,
-      useClass: RolesGuard,
-    },
-    AuthRepository,
-    RequestContextProvider,
-  ],
+    imports: [
+        TypeOrmModule.forRoot(config.database),
+        AuthModule,
+        UserModule,
+        BooksModule,
+        BasketModule,
+        OrderModule,
+    ],
+    controllers: [AppController],
+    providers: [
+        {
+            provide: APP_GUARD,
+            useClass: RolesGuard,
+        },
+        AuthRepository,
+        RequestContextProvider,
+    ],
 })
-
 export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(RequestContextMiddleware)
-      .exclude({path: 'books', method:RequestMethod.GET},
-      {path:'books/review', method: RequestMethod.POST}
-      )
-      .forRoutes(BooksController, BasketController, OrderController);
-  }
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(RequestContextMiddleware)
+            .exclude(
+                { path: 'books', method: RequestMethod.GET },
+                { path: 'books/review', method: RequestMethod.POST },
+            )
+            .forRoutes(BooksController, BasketController, OrderController);
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,49 @@
-import "reflect-metadata";
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { ValidationPipe } from "@nestjs/common";
-import * as cookieParser from "cookie-parser";
-import { createConnection } from "typeorm";
-import { config, entities } from "./config";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import 'reflect-metadata';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { createConnection } from 'typeorm';
+import { config, entities } from './config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const swagger = new DocumentBuilder()
-    .setTitle('Project-sql')
-    .setDescription('The Project-sql API description')
-    .setVersion('1.0')
-    .addTag('Endpoints')
-    .addBearerAuth(
-      {type:'http', scheme:'bearer', bearerFormat:'Token'},
-      'acces-token',
-    )
-    .build();
-  const document = SwaggerModule.createDocument(app, swagger);
-  SwaggerModule.setup('api', app, document);
-  app.use(cookieParser());
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      transformOptions: { enableImplicitConversion: false },
+    const app = await NestFactory.create(AppModule);
+    const swagger = new DocumentBuilder()
+        .setTitle('Project-sql')
+        .setDescription('The Project-sql API description')
+        .setVersion('1.0')
+        .addTag('Endpoints')
+        .addBearerAuth(
+            { type: 'http', scheme: 'bearer', bearerFormat: 'Token' },
+            'access-token',
+        )
+        .build();
+    const document = SwaggerModule.createDocument(app, swagger);
+    SwaggerModule.setup('api', app, document);
+    app.use(cookieParser());
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            transform: true,
+            transformOptions: { enableImplicitConversion: false },
+        }),
+    );
+    createConnection({
+        name: 'new',
+        type: 'postgres',
+        host: config.database.host,
+        port: 5432,
+        username: config.database.username,
+        password: config.database.password,
+        database: config.database.database,
+        entities: entities,
+        synchronize: false,
     })
-  );
-  createConnection({
-    name: "new",
-    type: "postgres",
-    host: config.database.host,
-    port: 5432,
-    username: config.database.username,
-    password: config.database.password,
-    database: config.database.database,
-    entities: entities,
-    synchronize: false,
-  })
-    .then((connection) => {
-      connection.runMigrations();
-    })
-    .catch((error) => console.log(error));
-  await app.listen(3000);
+        .then((connection) => {
+            connection.runMigrations();
+        })
+        .catch((error) => console.log(error));
+    await app.listen(3000);
 }
 bootstrap();
