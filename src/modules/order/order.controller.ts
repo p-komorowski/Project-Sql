c91c90b5ad55637cbfc6d02d612a9a0eb1ba0c6e
p-komorowski <pkomorowski29@gmail.com>
2021-11-08 15:01:42 +0100

    style: better looking code

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -1,39 +1,36 @@
-import { Controller, Get, Post, UseGuards } from "@nestjs/common";
-import {ApiBearerAuth, ApiOperation, ApiResponse, ApiTags, ApiUnauthorizedResponse } from "@nestjs/swagger";
-import { Roles } from "../auth/decorators/role.decorator";
-import { JwtAuthGuard } from "../auth/strategy/jwt-auth.guard";
-import { Role } from "../auth/strategy/models/role.enum";
-import { RolesGuard } from "../auth/strategy/roles.guard";
-import { Order } from "./entity/order.entity";
-import { OrderService } from "./order.service";
+import { Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { Roles } from '../auth/decorators/role.decorator';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Role } from '../user/enum/role.enum';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Order } from './entity/order.entity';
+import { OrderService } from './order.service';
 
-@ApiTags("Order")
-@Controller("order")
+@ApiTags('Order')
+@Controller('order')
 export class OrderController {
-  constructor(private orderService: OrderService) {}
+    constructor(private orderService: OrderService) {}
 
-  
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Post()
-  @Roles(Role.User)
-  @ApiBearerAuth()
-  @ApiOperation({ summary: 'Create order for logged yser' })
-  @ApiResponse({ status: 200, description: 'Order created.' })
-  @ApiUnauthorizedResponse({ description: 'User not logged in.' })
-  async addBasketToOrder() {
-    await this.orderService.createOrder();
-  }
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Post()
+    @Roles(Role.User)
+    @ApiBearerAuth()
+    @ApiOperation({ summary: 'Create order for logged yser' })
+    @ApiResponse({ status: 200, description: 'Order created.' })
+    @ApiUnauthorizedResponse({ description: 'User not logged in.' })
+    async addBasketToOrder() {
+        await this.orderService.createOrder();
+    }
 
-  
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Get()
-  @Roles(Role.User)
-  @ApiBearerAuth()
-  @ApiOperation({ summary: 'Show list of places orders' })
-  @ApiResponse({ status: 200, description: 'Order list shown' })
-  @ApiUnauthorizedResponse({ description: 'User not logged in.' })
-  async getAllOrders():Promise<Order[]>{
-   return await this.orderService.getAllOrders()
-  }
-  
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Get()
+    @Roles(Role.Moderator)
+    @ApiBearerAuth()
+    @ApiOperation({ summary: 'Show list of places orders' })
+    @ApiResponse({ status: 200, description: 'Order list shown' })
+    @ApiUnauthorizedResponse({ description: 'User not logged in.' })
+    async getAllOrders(): Promise<Order[]> {
+        return this.orderService.getAllOrders();
+    }
 }
