758add128d67e48e8b1e14bacc8814f3a263d12b
p-komorowski <pkomorowski29@gmail.com>
2021-11-08 14:45:54 +0100

    feat: add index file

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,17 +1,17 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { Exclude, Expose } from "class-transformer";
-import { IsEmail, IsString } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { Exclude, Expose } from 'class-transformer';
+import { IsEmail, IsString } from 'class-validator';
 
 @Exclude()
 export class LoginDto {
-  @Expose()
-  @IsEmail()
-  @IsString()
-  @ApiProperty({type: String, description: 'email'})
-  email: string;
+    @Expose()
+    @IsEmail()
+    @IsString()
+    @ApiProperty({ type: String, description: 'email' })
+    email: string;
 
-  @Expose()
-  @IsString()
-  @ApiProperty({type: String, description: 'password'})
-  password: string;
+    @Expose()
+    @IsString()
+    @ApiProperty({ type: String, description: 'password' })
+    password: string;
 }
--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -1,27 +1,26 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { Exclude, Expose } from "class-transformer";
-import { IsEmail, IsString } from "class-validator";
-import { Role } from "../strategy/models/role.enum";
+import { ApiProperty } from '@nestjs/swagger';
+import { Exclude, Expose } from 'class-transformer';
+import { IsEmail, IsString } from 'class-validator';
+import { Role } from '../../user/enum/role.enum';
 
 @Exclude()
 export class RegisterDto {
-  @Expose()
-  @IsEmail()
-  @ApiProperty({type: String, description: 'email'})
-  email: string;
+    @Expose()
+    @IsEmail()
+    @ApiProperty({ type: String, description: 'email' })
+    email: string;
 
-  @Expose()
-  @IsString()
-  @ApiProperty({type: String, description: 'password'})
-  password: string;
+    @Expose()
+    @IsString()
+    @ApiProperty({ type: String, description: 'password' })
+    password: string;
 
-  @Expose()
-  @IsString()
-  @ApiProperty({type: String, description: 'name'})
-  name: string;
+    @Expose()
+    @IsString()
+    @ApiProperty({ type: String, description: 'name' })
+    name: string;
 
-  @Expose()
-  @IsString()
-  role: Role[];
-  
+    @Expose()
+    @IsString()
+    role: Role[];
 }
