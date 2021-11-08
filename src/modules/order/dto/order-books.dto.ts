ed3050f943057c380a1bff70ed46f2a5aec11f91
p-komorowski <pkomorowski29@gmail.com>
2021-11-08 14:59:07 +0100

    style: rename dto file

---
+++ src/modules/order/dto/order-books.dto.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity()
+export class OrderBooks {
+    @PrimaryColumn()
+    id: string = uuid();
+
+    @Column({ name: 'order_id' })
+    orderId: string = uuid();
+
+    @Column()
+    IBSN: string;
+
+    @Column()
+    count: number;
+}
