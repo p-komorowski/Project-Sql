572a102ed4db4c73248c34ba16be6e189d0eff1c
p-komorowski <pkomorowski29@gmail.com>
2021-11-08 14:56:02 +0100

    style: better looking code

--- src/modules/book/entity/book.entity.ts
+++ src/modules/book/entity/book.entity.ts
@@ -1,42 +1,42 @@
-import { OrderBooks } from "../../order/dto/order_books.dto";
-import { Entity, Column, PrimaryColumn, ManyToOne, OneToMany, JoinColumn, DeepPartial } from "typeorm";
-import { ApiProperty } from "@nestjs/swagger";
-import { BookInterface } from "../interface/book.interface";
-import { BasketBook } from "../../basket/entities/basket_book.entity";
-import { Review } from "../../review/entity/review.entity";
+import { OrderBooks } from '../../order/dto/order_books.dto';
+import { Entity, Column, PrimaryColumn, ManyToOne, OneToMany, JoinColumn, DeepPartial } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { BookInterface } from '../interface/book.interface';
+import { BasketBook } from '../../basket/entities/basket-book.entity';
+import { Review } from '../../review/entity/review.entity';
 
 @Entity()
 export class Book {
-  constructor(book: DeepPartial<BookInterface>) {
-    Object.assign(this, book);
-  }
-  @ApiProperty({ description: "IBSN of book" })
-  @PrimaryColumn()
-  IBSN: string;
+    constructor(book: DeepPartial<BookInterface>) {
+        Object.assign(this, book);
+    }
+    @ApiProperty({ description: 'IBSN of book' })
+    @PrimaryColumn()
+    IBSN: string;
 
-  @ApiProperty({ description: "Title of book" })
-  @Column({ nullable: true })
-  title: string;
+    @ApiProperty({ description: 'Title of book' })
+    @Column({ nullable: true })
+    title: string;
 
-  @ApiProperty({ description: "price of book" })
-  @Column({ nullable: true })
-  price: number;
+    @ApiProperty({ description: 'price of book' })
+    @Column({ nullable: true, type: 'decimal', precision: 5, scale: 2 })
+    price: number;
 
-  @ApiProperty({ description: "Name of author" })
-  @Column({ nullable: true })
-  author: string;
+    @ApiProperty({ description: 'Name of author' })
+    @Column({ nullable: true })
+    author: string;
 
-  @ApiProperty({ description: "count of book" })
-  @Column({ nullable: true })
-  count: number;
+    @ApiProperty({ description: 'count of book' })
+    @Column({ nullable: true })
+    count: number;
 
-  @OneToMany(() => BasketBook, (basketBook) => basketBook.book)
-  basketBook: BasketBook;
+    @OneToMany(() => BasketBook, (basketBook) => basketBook.book)
+    basketBook: BasketBook;
 
-  @ManyToOne(() => OrderBooks, (orderBooks) => orderBooks.id)
-  orderBooks: OrderBooks[];
+    @ManyToOne(() => OrderBooks, (orderBooks) => orderBooks.id)
+    orderBooks: OrderBooks[];
 
-  @OneToMany(() => Review, (review) => review.book)
-  @JoinColumn()
-  review: Review[];
+    @OneToMany(() => Review, (review) => review.book)
+    @JoinColumn()
+    review: Review[];
 }
