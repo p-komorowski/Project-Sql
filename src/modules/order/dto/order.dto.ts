53f0951a885afb61a50c2e66722c86260b9f1bc7
p-komorowski <pkomorowski29@gmail.com>
2021-11-08 14:59:17 +0100

    style: better looking code

--- src/modules/order/dto/order.dto.ts
+++ src/modules/order/dto/order.dto.ts
@@ -1,12 +1,11 @@
-import { Column, Entity } from "typeorm";
-import { v4 as uuid } from "uuid";
+import { Column, Entity } from 'typeorm';
+import { v4 as uuid } from 'uuid';
 
 @Entity()
 export class OrderBooksDto {
-  @Column({ name: "order_id" })
-  orderId: string = uuid();
+    @Column({ name: 'order_id' })
+    orderId: string = uuid();
 
-  @Column({ name: "user_id" })
-  userId: string = uuid();
-  
+    @Column({ name: 'user_id' })
+    userId: string = uuid();
 }
