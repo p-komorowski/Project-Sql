68a03ce56ada7fd3ea9c6163e3a3ec502c2cbded
p-komorowski <pkomorowski29@gmail.com>
2021-11-08 15:00:23 +0100

    style:better looking code

--- src/modules/order/entity/order.entity.ts
+++ src/modules/order/entity/order.entity.ts
@@ -1,18 +1,17 @@
-import { Customer } from "../../user/entities";
-import { Entity, PrimaryColumn, JoinColumn, OneToOne } from "typeorm";
-import { v4 as uuid } from "uuid";
-import { Basket } from "../../basket/entities/basket.entity";
+import { Customer } from '../../user/entities';
+import { Entity, PrimaryColumn, JoinColumn, OneToOne } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { Basket } from '../../basket/entities/basket.entity';
 
 @Entity()
 export class Order {
-  @PrimaryColumn()
-  id: string = uuid();
+    @PrimaryColumn()
+    id: string = uuid();
 
-  @OneToOne(() => Basket, (basket) => basket.order)
-  @JoinColumn()
-  basket: Basket;
+    @OneToOne(() => Basket, (basket) => basket.order)
+    @JoinColumn()
+    basket: Basket;
 
-  @OneToOne(() => Customer, (user) => user.order)
-  user: Customer;
-
+    @OneToOne(() => Customer, (user) => user.order)
+    user: Customer;
 }
