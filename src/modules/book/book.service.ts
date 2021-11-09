293fedb0e8c0de2cb98217d022dcaa839580fd4a
p-komorowski <pkomorowski29@gmail.com>
2021-11-09 12:46:29 +0100

    refactor: move review methods to reviewService

--- src/modules/book/book.service.ts
+++ src/modules/book/book.service.ts
@@ -3,20 +3,14 @@ import { BooksRepository } from './repository/book.repository';
 import { Book } from './entity/book.entity';
 import { Connection } from 'typeorm';
 import { BookDto } from './dto/book.dto';
-import { ReviewRepository } from '../review/repository/review.repository';
-import { ReviewDto } from '../review/dto/review.dto';
-import { Review } from '../review/entity/review.entity';
 
 @Injectable()
 export class BooksService {
     logger: Logger;
     private booksRepository: BooksRepository;
-    private reviewRepository: ReviewRepository;
     constructor(private readonly connection: Connection) {
         this.booksRepository =
             this.connection.getCustomRepository(BooksRepository);
-        this.reviewRepository =
-            this.connection.getCustomRepository(ReviewRepository);
         this.logger = new Logger(BooksService.name);
     }
 
@@ -24,10 +18,10 @@ export class BooksService {
         return this.booksRepository.save(newProduct);
     }
 
-    public async getProducts(page: number = 1): Promise<Book[]> {
+    public async getProducts(page: number = 1, take: number): Promise<Book[]> {
         return this.booksRepository.find({
             relations: ['review'],
-            take: 5,
+            take: take,
             skip: 2 * (page - 1),
         });
     }
@@ -44,23 +38,8 @@ export class BooksService {
         });
         if (!book) {
             throw new UnauthorizedException('cannot find book');
-        } else {
-            return book;
         }
-    }
-
-    async addReviewToBook(dto: ReviewDto) {
-        const book = await this.getBook(dto.IBSN);
-        if (!book) {
-            throw new UnauthorizedException('book does not exist');
-        }
-
-        const newReview = await this.reviewRepository.create({
-            review: dto.review,
-        });
-        newReview.book = book;
-
-        return this.reviewRepository.save(newReview);
+        return book;
     }
 
     async changePriceOfBook(IBSN: string, price: number) {
@@ -75,15 +54,5 @@ export class BooksService {
 
     async deleteProduct(IBSN: string): Promise<void> {
         await this.booksRepository.delete(IBSN);
-    }
-
-    async deleteReviews(IBSN: string, review_ids: string[]): Promise<Review[]> {
-        const reviews = await this.reviewRepository
-            .createQueryBuilder('review')
-            .where('review.id IN (:...review)', { review: review_ids })
-            .andWhere('review.bookIBSN = :IBSN', { IBSN: IBSN })
-            .getMany();
-
-        return this.reviewRepository.remove(reviews);
     }
 }
