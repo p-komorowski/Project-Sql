317f5e196c7f811991b5b77cca25607758338605
p-komorowski <pkomorowski29@gmail.com>
2021-11-26 15:04:39 +0100

    refactor: add new export

--- src/modules/book/dto/index.ts
+++ src/modules/book/dto/index.ts
@@ -2,3 +2,4 @@ export * from './book-price.dto';
 export * from './book.dto';
 export * from './book-response.dto';
 export * from './price-history-response.dto';
+export * from './book-response-order';
