7c64a697999d7df11025a391ebfaead672de2cbf
p-komorowski <pkomorowski29@gmail.com>
2021-11-26 15:03:54 +0100

    refactor: change bookDescription type

--- src/modules/basket/dto/basket-book-response.dto.ts
+++ src/modules/basket/dto/basket-book-response.dto.ts
@@ -1,11 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Exclude, Expose, Type } from 'class-transformer';
-import { BookResponseDto } from '../../book/dto';
+import { BookOrderResponseDto } from '../../book/dto';
 
 @Exclude()
 export class BasketBookResponseDto {
+  @Expose({ name: 'count' })
+  @ApiProperty({ type: String, description: 'Count of book in basket' })
+  countOfBookInBasket: string;
+
   @Expose({ name: 'book' })
   @ApiProperty({ type: String, description: 'id' })
-  @Type(() => BookResponseDto)
-  bookDescription: BookResponseDto;
+  @Type(() => BookOrderResponseDto)
+  bookDescription: BookOrderResponseDto;
 }
